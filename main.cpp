83fb6a0302a90baf38d7ed408ab62f0050338af4
Johan Froberg <johfr198@student.liu.se>
2019-03-12 08:52:26 +0100

    Added rotation in main

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
-
+#include "./sgct.h"
+#include "./sgct/include/sgct/Engine.h"
 #include <iostream>
 #include "sgct.h"
 
 sgct::Engine * gEngine;
 
 void myDrawFun();
+void myPreSyncFun();
+void myEncodeFun();
+void myDecodeFun();
+void keyCallback(int key, int action);
 
-int main( int argc, char* argv[] ){
-    
-    std::cout<<"Hello world"<< std::endl;
+sgct::SharedDouble curr_time(0.0);
+
+float speed = 0.0f;
+
+int main(int argc, char* argv[])
+{
     
     // Allocate
-    gEngine = new sgct::Engine( argc, argv );
+    gEngine = new sgct::Engine(argc, argv);
     
     // Bind your functions
-    gEngine->setDrawFunction( myDrawFun );
+    gEngine->setDrawFunction(myDrawFun);
+    gEngine->setPreSyncFunction(myPreSyncFun);
+    gEngine->setKeyboardCallbackFunction(keyCallback);
+    sgct::SharedData::instance()->setEncodeFunction(myEncodeFun);
+    sgct::SharedData::instance()->setDecodeFunction(myDecodeFun);
     
     // Init the engine
-    if( !gEngine->init() )
+    if (!gEngine->init())
     {
         delete gEngine;
         return EXIT_FAILURE;
@@ -30,11 +42,13 @@ int main( int argc, char* argv[] ){
     delete gEngine;
     
     // Exit program
-    exit( EXIT_SUCCESS );
+    exit(EXIT_SUCCESS);
 }
 
 void myDrawFun()
 {
+    glRotatef(static_cast<float>(curr_time.getVal()) * speed, 0.0f, 1.0f, 0.0f);
+    
     //render a single triangle
     glBegin(GL_TRIANGLES);
     glColor3f(1.0f, 0.0f, 0.0f); //Red
@@ -46,4 +60,42 @@ void myDrawFun()
     glColor3f(0.0f, 0.0f, 1.0f); //Blue
     glVertex3f(0.5f, -0.5f, 0.0f);
     glEnd();
+}
+
+void myPreSyncFun()
+{
+    //set the time only on the master
+    if (gEngine->isMaster())
+    {
+        //get the time in seconds
+        curr_time.setVal(sgct::Engine::getTime());
+    }
+}
+
+void myEncodeFun()
+{
+    sgct::SharedData::instance()->writeDouble(&curr_time);
+}
+
+void myDecodeFun()
+{
+    sgct::SharedData::instance()->readDouble(&curr_time);
+}
+
+void keyCallback(int key, int action)
+{
+    if( gEngine->isMaster() )
+    {
+        switch( key )
+        {
+            case SGCT_KEY_A:
+                if(action == SGCT_PRESS)
+                    speed = 20.0f;
+                break;
+            case SGCT_KEY_S:
+                if(action == SGCT_PRESS)
+                    speed = -20.0f;
+                break;
+        }
+    }
 }

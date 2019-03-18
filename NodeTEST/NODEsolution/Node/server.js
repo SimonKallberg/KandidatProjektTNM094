81e6c5fd70bc01c116a259d37abdc50b932a0f6b
Petra <gunnarsdotter@live.com>
2019-03-18 10:12:16 +0100

    Small uppdate with name and css-style

--- NodeTEST/NODEsolution/Node/server.js
+++ NodeTEST/NODEsolution/Node/server.js
@@ -1,29 +1,43 @@
-'use strict';
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-var port = process.env.PORT || 1337;
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n' + res.connection.localPort);
+app.use(express.static(__dirname + '/public'));
+var user = [];
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/Klient.html');
 });
 
-server.listen(1337, function () {
-    console.log('server is listening on port: ' + port);
-});
+io.on('connection', function (socket) {
+    var name;
+    console.log('a user connected');
+    
+    socket.emit('getName');
 
-var wsServer = new WebSocketServer({
-    httpServer: server
-});
+    socket.on('userName', function (name1) {
+        name = name1;
+        user.push(name);
+        console.log(name);
+        socket.emit('changeBackground');
+    });
 
-wsServer.on('request', function (request) {
-    var connection = request.accept(null, request.origin);
-    console.log('client connected');
-    connection.on('message', function (message) {
-        if (message.type === 'utf8') {
-            console.log(message);
-        }
+    socket.on('message', function (arg) {
+        console.log(arg , name);
+    });
+
+    socket.on('disconnect', function () {
+        user.pop(name);
+        console.log(name, ' disconnected', user.length);
     });
 });
 
 
+http.listen(process.env.PORT || 1337, function () {
+    console.log('listening on server');
+});
+
+
+//.emit för att skicka
+//.on för att ta emot
